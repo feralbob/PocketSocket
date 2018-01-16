eb80f038d03d591dbead133efeb771221679c6af
Jeff Ward <jeff.ward@willowtreeapps.com>
2018-01-16 17:45:20 -0500

    Adding public framework for carthage support.

---
+++ PocketSocket-Framework/PocketSocket.h
@@ -0,0 +1,19 @@
+//
+//  PocketSocket_Framework.h
+//  PocketSocket-Framework
+//
+//  Created by Jeff Ward on 1/16/18.
+//  Copyright © 2018 Zwopple Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PocketSocket_Framework.
+FOUNDATION_EXPORT double PocketSocket_FrameworkVersionNumber;
+
+//! Project version string for PocketSocket_Framework.
+FOUNDATION_EXPORT const unsigned char PocketSocket_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PocketSocket_Framework/PublicHeader.h>
+
+
