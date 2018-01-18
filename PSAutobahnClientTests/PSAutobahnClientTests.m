66ca40c4068722c00a2f2998d6de57bc8df515b8
Jeff Ward <jeff.ward@willowtreeapps.com>
2018-01-18 10:09:35 -0500

    Restructure tests to show name and pass / fail in Xcode.

--- PSAutobahnClientTests/PSAutobahnClientTests.m
+++ PSAutobahnClientTests/PSAutobahnClientTests.m
@@ -15,145 +15,73 @@
 #import <XCTest/XCTest.h>
 #import "PSAutobahnClientWebSocketOperation.h"
 
-@interface PSAutobahnTests : XCTestCase {
+@interface PSAutobahnTestSuite : XCTestCase {
 }
 
-@property (nonatomic, strong) NSURL *autobahnURL;
-@property (nonatomic, strong) NSNumber *cachedTestCaseCount;
-@property (nonatomic, strong) NSString *desc;
+@property(nonatomic, strong) NSNumber* caseNumber;
+@property(nonatomic, strong) NSDictionary* info;
+@property(nonatomic, readonly) NSString* name;
 
 @end
 
-@implementation PSAutobahnTests
+@implementation PSAutobahnTestSuite
 
-#pragma mark - Properties
-
-- (NSURL *)autobahnURL {
-    if(!_autobahnURL) {
-        _autobahnURL = [NSURL URLWithString:@"ws://localhost:9001/"];
-    }
-    return _autobahnURL;
-}
-- (NSString *)agent {
-    return @"com.zwopple.PSWebSocket";
-}
-- (NSString *)description {
-    if(self.desc) {
-        return self.desc;
-    }
-    return @"PocketSocket Autobahn Test Harness";
-}
-- (BOOL)isEmpty {
-    return NO;
-}
-- (NSUInteger)testCaseCount {
-    if(self.invocation) {
-        return [super testCaseCount];
-    }
-    if(!self.cachedTestCaseCount) {
-        self.cachedTestCaseCount = @([self autobahnFetchTestCaseCount]);
-    }
-    return self.cachedTestCaseCount.unsignedIntegerValue;
-}
-
-#pragma mark - Initialization
-
-+ (id)defaultTestSuite {
-    return [[[self class] alloc] init];
-}
-- (id)initWithInvocation:(NSInvocation *)invocation desc:(NSString *)desc {
-    self.desc = desc;
-    if((self = [super initWithInvocation:invocation])) {
-    }
-    return self;
-}
-
-#pragma mark - Testing
-
-- (void)performTest:(XCTestRun *)run {
-    if(self.invocation) {
-        NSLog(@"[PSAutobahnClientTests][EXECUTE]: %@", self.desc);
-        [super performTest:run];
-        return;
-    }
-    [run start];
-    for(NSUInteger i = 1; i <= run.test.testCaseCount; ++i) {
-        SEL selector = @selector(performTestNumber:);
-        NSMethodSignature *signature = [self methodSignatureForSelector:selector];
++(id)defaultTestSuite {
+    XCTestSuite* suite = [[XCTestSuite alloc] initWithName:NSStringFromClass(self)];
+    NSLog(@"Fetching test count from server");
+    NSUInteger caseCount = [self autobahnFetchTestCaseCount];
+    NSLog(@"Found %d tests. Running fetching test info.", caseCount);
+    for(NSUInteger i = 1; i <= caseCount; ++i) {
+        PSAutobahnTestSuite* test = [[PSAutobahnTestSuite alloc] initWithInvocation:nil caseNumber:@(i)];
+        
+        SEL selector = @selector(test_autobahn_case_number:);
+        NSMethodSignature *signature = [test methodSignatureForSelector:selector];
         NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
         invocation.selector = selector;
-        invocation.target = self;
+        invocation.target = test;
         [invocation setArgument:&i atIndex:2];
+        test.invocation = invocation;
         
         NSDictionary *info = [self autobahnFetchTestCaseInfoForNumber:i];
-        NSString *desc = [NSString stringWithFormat:@"%@ – %@", info[@"id"], info[@"description"]];
-        
-        XCTestCase *testCase = [[[self class] alloc] initWithInvocation:invocation desc:desc];
-        XCTestCaseRun *testRun = [[XCTestCaseRun alloc] initWithTest:testCase];
-        [testCase performTest:testRun];
+        test.info = info;
+        //NSString* name = [test name];
+        [suite addTest:test];
     }
-    [self autobahnUpdateReports];
-    [run stop];
-}
-- (void)performTestNumber:(NSUInteger)caseNumber {
-    NSDictionary *results = [self autobahnPerformTestCaseNumber:caseNumber];
-    XCTAssertEqualObjects(@"OK", results[@"behavior"], @"Test behavior should have been ok, instead got: %@", results[@"behavior"]);
+    
+    // Update reports
+    PSAutobahnTestSuite* test = [[PSAutobahnTestSuite alloc] initWithInvocation:nil caseNumber:nil];
+
+    SEL selector = @selector(autobahnUpdateReports);
+    NSMethodSignature* signature = [test methodSignatureForSelector:selector];
+    NSInvocation* invocation = [NSInvocation invocationWithMethodSignature:signature];
+    invocation.selector = selector;
+    invocation.target = test;
+    test.invocation = invocation;
+    [suite addTest:test];
+    
+    return suite;
 }
 
-#pragma mark - Autobahn Operations
-
-- (NSDictionary *)autobahnPerformTestCaseNumber:(NSInteger)number {
-    NSString *extra = [NSString stringWithFormat:@"/runCase?case=%@&agent=%@", @(number), self.agent];
-    NSURL *URL = [NSURL URLWithString:extra relativeToURL:self.autobahnURL];
-    PSAutobahnClientWebSocketOperation *op = [[PSAutobahnClientWebSocketOperation alloc] initWithURL:URL];
-    op.echo = YES;
-    [self runOperation:op timeout:60.0];
-    XCTAssertNil(op.error, @"Should have successfully run the test case. Instead got error %@", op.error);
-    return [self autobahnFetchTestCaseStatusForNumber:number];
-}
-- (NSUInteger)autobahnFetchTestCaseCount {
++ (NSUInteger)autobahnFetchTestCaseCount {
     NSURL *URL = [self.autobahnURL URLByAppendingPathComponent:@"getCaseCount"];
     PSAutobahnClientWebSocketOperation *op = [[PSAutobahnClientWebSocketOperation alloc] initWithURL:URL];
     [self runOperation:op timeout:60.0];
-    XCTAssertNil(op.error, @"Should have successfully returned the number of testCases. Instead got error %@", op.error);
+    //XCTAssertNil(op.error, @"Should have successfully returned the number of testCases. Instead got error %@", op.error);
     return [op.message integerValue];
 }
-- (NSDictionary *)autobahnFetchTestCaseInfoForNumber:(NSUInteger)number {
+
++ (NSDictionary *)autobahnFetchTestCaseInfoForNumber:(NSUInteger)number {
     NSString *extra = [NSString stringWithFormat:@"/getCaseInfo?case=%@", @(number)];
     NSURL *URL = [NSURL URLWithString:extra relativeToURL:self.autobahnURL];
     PSAutobahnClientWebSocketOperation *op = [[PSAutobahnClientWebSocketOperation alloc] initWithURL:URL];
-    [self runOperation:op timeout:60.0];
-    XCTAssertNil(op.error, @"Should have successfully returned the case info. Instead got error %@", op.error);
+    [PSAutobahnTestSuite runOperation:op timeout:60.0];
+    //XCTAssertNil(op.error, @"Should have successfully returned the case info. Instead got error %@", op.error);
     NSDictionary *info = [NSJSONSerialization JSONObjectWithData:[op.message dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
-    XCTAssertNil(info, @"Should have successfully deserialized message into dictionary.");
+    //XCTAssertNotNil(info, @"Should have successfully deserialized message into dictionary.");
     return info;
 }
-- (NSDictionary *)autobahnFetchTestCaseStatusForNumber:(NSUInteger)number {
-    NSString *extra = [NSString stringWithFormat:@"/getCaseStatus?case=%@&agent=%@", @(number), self.agent];
-    NSURL *URL = [NSURL URLWithString:extra relativeToURL:self.autobahnURL];
-    PSAutobahnClientWebSocketOperation *op = [[PSAutobahnClientWebSocketOperation alloc] initWithURL:URL];
-    [self runOperation:op timeout:60.0];
-    XCTAssertNil(op.error, @"Should have successfully returned the case status. Instead got error %@", op.error);
-    NSDictionary *info = [NSJSONSerialization JSONObjectWithData:[op.message dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
-    XCTAssertNil(info, @"Should have successfully deserialized message into dictionary.");
-    return info;
-}
-- (void)autobahnUpdateReports {
-    NSString *extra = [NSString stringWithFormat:@"/updateReports?agent=%@", self.agent];
-    NSURL *URL = [NSURL URLWithString:extra relativeToURL:self.autobahnURL];
-    PSAutobahnClientWebSocketOperation *op = [[PSAutobahnClientWebSocketOperation alloc] initWithURL:URL];
-    [self runOperation:op timeout:60.0];
-    XCTAssertNil(op.error, @"Should have successfully updated the reports. Instead got error %@", op.error);
-}
 
-#pragma mark - Failures
-
-- (void)recordFailureWithDescription:(NSString *)description inFile:(NSString *)filename atLine:(NSUInteger)lineNumber expected:(BOOL)expected {
-    if(!expected) {
-        NSLog(@"[PSAutobahnClientTests][FAIL]: %@", description);
-    }
-}
-- (void)runOperation:(NSOperation *)operation timeout:(NSTimeInterval)timeout {
++ (void)runOperation:(NSOperation *)operation timeout:(NSTimeInterval)timeout {
     static NSOperationQueue *queue = nil;
     static dispatch_once_t once = 0;
     dispatch_once(&once, ^{
@@ -172,8 +100,74 @@
     [op addDependency:operation];
     [queue addOperation:operation];
     [queue addOperation:op];
-    XCTAssertTrue([condition waitUntilDate:[NSDate dateWithTimeIntervalSinceNow:timeout]], @"Timed out");
+    [condition waitUntilDate:[NSDate dateWithTimeIntervalSinceNow:timeout]];
     [condition unlock];
 }
 
++ (NSURL *)autobahnURL {
+    return [NSURL URLWithString:@"ws://localhost:9001/"];
+}
+
++ (NSString *)agent {
+    return @"com.zwopple.PSWebSocket";
+}
+
+-(instancetype)initWithInvocation:(NSInvocation*)invocation caseNumber:(NSNumber*)caseNumber {
+    if(self = [super initWithInvocation:invocation]) {
+        self.caseNumber = caseNumber;
+    }
+    
+    return self;
+}
+
+#pragma mark - Autobahn Operations
+
+- (NSDictionary *)test_autobahn_case_number:(NSUInteger)number {
+    NSString *extra = [NSString stringWithFormat:@"/runCase?case=%@&agent=%@", @(number), [PSAutobahnTestSuite agent]];
+    NSURL *URL = [NSURL URLWithString:extra relativeToURL:[PSAutobahnTestSuite autobahnURL]];
+    PSAutobahnClientWebSocketOperation *op = [[PSAutobahnClientWebSocketOperation alloc] initWithURL:URL];
+    op.echo = YES;
+    [PSAutobahnTestSuite runOperation:op timeout:60.0];
+    XCTAssertNil(op.error, @"Should have successfully run the test case. Instead got error %@", op.error);
+    NSDictionary* results = [self autobahnFetchTestCaseStatusForNumber:number];
+    XCTAssertEqualObjects(@"OK", results[@"behavior"], @"Test behavior should have been ok, instead got: %@", results[@"behavior"]);
+}
+
+- (NSDictionary *)autobahnFetchTestCaseStatusForNumber:(NSUInteger)number {
+    NSString *extra = [NSString stringWithFormat:@"/getCaseStatus?case=%@&agent=%@", @(number),  [PSAutobahnTestSuite agent]];
+    NSURL *URL = [NSURL URLWithString:extra relativeToURL:[PSAutobahnTestSuite autobahnURL]];
+    PSAutobahnClientWebSocketOperation *op = [[PSAutobahnClientWebSocketOperation alloc] initWithURL:URL];
+    [PSAutobahnTestSuite runOperation:op timeout:60.0];
+    XCTAssertNil(op.error, @"Should have successfully returned the case status. Instead got error %@", op.error);
+    NSDictionary *info = [NSJSONSerialization JSONObjectWithData:[op.message dataUsingEncoding:NSUTF8StringEncoding] options:0 error:nil];
+    XCTAssertNotNil(info, @"Should have successfully deserialized message into dictionary.");
+    return info;
+}
+
+- (void)autobahnUpdateReports {
+    NSString *extra = [NSString stringWithFormat:@"/updateReports?agent=%@",  [PSAutobahnTestSuite agent]];
+    NSURL *URL = [NSURL URLWithString:extra relativeToURL:[PSAutobahnTestSuite autobahnURL]];
+    PSAutobahnClientWebSocketOperation *op = [[PSAutobahnClientWebSocketOperation alloc] initWithURL:URL];
+    [PSAutobahnTestSuite runOperation:op timeout:60.0];
+    XCTAssertNil(op.error, @"Should have successfully updated the reports. Instead got error %@", op.error);
+}
+
+- (NSInvocation*)invocation {
+    return [super invocation];
+}
+
+- (NSString*)languageAgnosticTestMethodName {
+    NSString* desc = [NSString stringWithFormat:@"%@ - %@", self.info[@"id"], self.info[@"description"]];
+    return desc;
+}
+
+- (NSString*)name {
+    NSString* desc = [super name];
+    //NSString *desc = [NSString stringWithFormat:@"%@ – %@", self.info[@"id"], self.info[@"description"]];
+    desc = [NSString stringWithFormat:@"%@ - %@", self.info[@"id"], self.info[@"description"]];
+    return desc;
+}
+
 @end
+
+
---
+++ PocketSocket.h
@@ -0,0 +1,18 @@
+//
+//  PocketSocket.h
+//  PocketSocket
+//
+//  Created by Jeff Ward on 1/17/18.
+//  Copyright © 2018 WillowTree, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PocketSocket.
+FOUNDATION_EXPORT double PocketSocketVersionNumber;
+
+//! Project version string for PocketSocket.
+FOUNDATION_EXPORT const unsigned char PocketSocketVersionString[];
+
+#import <PocketSocket/PSWebSocket.h>
+#import <PocketSocket/PSWebSocketServer.h>
